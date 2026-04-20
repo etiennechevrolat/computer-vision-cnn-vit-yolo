221b697a559f4c503d0b09c2ba020ca579150c75
NA <etienne.chevrolat@nissan.polytechnique.fr>
2026-04-20 21:03:50 +0200

    restructuration du code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+
 
 from torchvision import datasets, transforms, models 
 from cnn.models import CNN1, CNN2, CNN3, CNN4, LeNet, mycnn
